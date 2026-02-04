91d6b7dc90c3a9375241f1f0b1a99684d38b8385
Artur Avagyan <artavagy@c2r15s1.42yerevan.am>
2026-02-04 17:08:47 +0400

    #

---
+++ test.c
@@ -0,0 +1,20 @@
+ 619  echo "# Libft" >> README.md\ngit init\ngit add README.md\ngit commit -m "first commit"\ngit branch -M main\ngit remote add origin git@github.com:arthavagyan/Libft.git\ngit push -u origin main
+  620  git init
+  621  git status
+  622  git add .\ngit commit -m "Initial commit"\n
+  623  git branch -M main\ngit remote add origin git@github.com:arthavagyan/Libft.git\ngit push -u origin main
+  624  git remote set-url origin https://github.com/USERNAME/REPO.git\ngit push
+  625  git remote remove origin\ngit remote add origin git@github.com:arthavagyan/Libft.git\ngit push -u origin main\n
+  628  ./a
+  629  gcc main.c ft_*
+  630  ./a.out
+  631  norminette
+  632  pwd
+  633  git clone https://github.com/armannikoyan/francinette
+  634  rm -rf francinette
+  635  bash -c "$(curl -fsSL https://raw.github.com/armannikoyan/francinette/master/bin/install.sh)"
+  636  exit
+  637  source ~/.bashrc
+  638  source ~/.zshrc
+  650  la
+
