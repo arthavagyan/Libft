fd2d992853248c3bab6e8eff9fb95c6b8a750ba8
Artur Avagyan <artavagy@c1r1s2.42yerevan.am>
2026-02-07 16:27:22 +0400

    Feb 7 16:27

--- ft_substr.c
+++ ft_substr.c
@@ -6,12 +6,29 @@
 /*   By: artavagy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 19:50:05 by artavagy          #+#    #+#             */
-/*   Updated: 2026/02/05 17:59:10 by artavagy         ###   ########.fr       */
+/*   Updated: 2026/02/06 18:12:46 by artavagy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	
+	size_t	size_str;
+	size_t	size_re;
+	char	*s_point;
+	char	*result;
+
+	size_str = ft_strlen(s);
+	if (!s)
+		return (NULL);
+	s_point = (char *)s + start;
+	if (ft_strlen(s_point) < len)
+		size_re = ft_strlen(s_point) + 1;
+	else
+		size_re = len + 1;
+	result = malloc(reslen * sizeof(char));
+	if (!result)
+		return (NULL);
+	strlcpy(result, s_point, size_re);
+	return (result);
 }
