f173f26e97ad6dd373cc9db45469810c05e4152f
Artur Avagyan <artavagy@c2r16s3.42yerevan.am>
2026-02-13 03:21:24 +0400

    feb 13 03:21

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: artavagy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/07 22:22:33 by artavagy          #+#    #+#             */
-/*   Updated: 2026/02/07 22:45:24 by artavagy         ###   ########.fr       */
+/*   Updated: 2026/02/13 03:17:29 by artavagy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -18,9 +18,9 @@ char	*ft_strtrim(char const *s1, char const *set)
 	char	*res;
 
 	start_point = 0;
-	end_point = ft_strlen(s1);
 	if (!s1 || !set)
 		return (NULL);
+	end_point = ft_strlen(s1);
 	while (s1[start_point] && ft_strchr(set, s1[start_point]))
 		start_point++;
 	while (end_point > start_point && ft_strchr(set, s1[end_point - 1]))
